5f6aa64b089f589d23271f76cc4d7618cb069ff3
AJG-5090 <germainjosueadley977@gmail.com>
2024-12-07 22:55:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "r6JnhYxA#4-pJlbrmeiJ-kZNstb2GVHpSxyW6NA_13DLTUcKEyBw",
+    SESSION_ID: process.env.SESSION_ID || "jcpTxA5A#25Ihs9bDd8k2RRpFSgF1Kk62j99QZZmsZLIO_xkKe4w",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
