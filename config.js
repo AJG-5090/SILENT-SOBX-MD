060c6e24bbc86a0cbf063fd44d004ab0a103db17
AJG-5090 <germainjosueadley977@gmail.com>
2024-12-11 21:52:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "fB4jxChY#AwjhyGX8L6kdIyh1WToETPmJ_q4cThoyuKnOFRCU4G0",
+    SESSION_ID: process.env.SESSION_ID || "GEhGXKLZ#yVqCZmqaanhARBKU67LB87yt83aPILFAuWjHj8wrb2s",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
@@ -14,7 +14,7 @@ module.exports = {
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "true",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.pinimg.com/736x/09/60/e6/0960e63ede64968934087c3cdb4d7c84.jpg",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.pinimg.com/736x/dd/e1/13/dde1136ba955a91fee8d0d7802142083.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "Hii mother fucker senku i'm Alive my king 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
